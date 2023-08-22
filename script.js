2860cddb4b9aa82ef37b37be2197bb9c7f03ba84
Hanzalla Muzaffar <hanzialam034@gmail.com>
2023-08-22 22:09:32 +0500

    url shrinker completed without about and contact button.

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const long_url = document.getElementById('long-url');
+
+const long_url = document.getElementById('long_url');
 const submit_button = document.getElementById('submit_button');
+const short_url = document.getElementById('short_url');
+const copied_btn = document.getElementById('copied_button');
 
 
-submit_button.addEventListener('submit', getUrl);
-
-function getUrl(event) {
+submit_button.addEventListener("click", () => {
     event.preventDefault();
-    const urlValue = long_url.value;
-    console.log(urlValue);
+    let newUrl = long_url.value;
+    fetch(`https://api.shrtco.de/v2/shorten?url=${newUrl}`)
+        .then((resp) => resp.json())
+        .then((value) => {
+            short_url.value = value.result.short_link2;
+        })
+        .catch((error) => {
+            short_url.value = "Something went wrong!";
+        });
 
+});
+
+function updatedBtnText() {
+    copied_btn.innerHTML = "Copy";
 }
 
-submit_button.onclick = getUrl;
+copied_btn.addEventListener("click", () => {
+    event.preventDefault();
+    navigator.clipboard.writeText(short_url.value);
+    copied_btn.innerHTML = "Copied";
+
+    setTimeout(updatedBtnText, 1000);
+})
